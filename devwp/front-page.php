961da57696ea972fda913dbc446862ffab9c13b4
Myzwer <josh@onefortyfivedesign.com>
2020-12-01 17:15:46 -0500

    Got the time / date / location section done, just created a new scss file to house it since it was so specific.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -24,13 +24,13 @@ get_header(); ?>
                playsinline muted></video>
 
         <div class="viewport-header">
-            <div>
-                <div class="main-header"><div class="logo"><img
+
+                <div class="main-header">
+                    <div class="logo"><img
                             src="http://christmas-2020.local/wp-content/uploads/2020/12/AFC_WEBSITE_SCALED_TRANSPARENT-1.png"
-                            alt=""></div>
+                            alt="">
+                    </div>
                 </div>
-                <!--<h1 class="main-header">Sunday, December 20</h1>-->
-            </div>
         </div>
     </div>
 
@@ -59,98 +59,83 @@ get_header(); ?>
         </div>
     </div>
 
-<div class="full-width main-background">
-    <div class = "grid-container">
-        <div class="grid-x grid-padding-x">
-            <div class="large-12 cell">
-                <h1>Welcome to 6!</h1>
-
-                <button class="btn btn-v2">Button 5</button>
-                <div class="callout">
-                    <h3>We're stoked you want to try Foundation! <i class="fas fa-address-book"></i></h3>
-                    <p>To get going, this file (index.html) includes some basic styles you can modify, play around with, or totally destroy to get going.</p>
-                    <p>Once you've exhausted the fun in this document, you should check out:</p>
+    <div class="full-width main-background padding-bottom">
+        <div class = "grid-container ">
+            <div class="grid-x grid-padding-x grid-margin-x">
+                <div class="small-12 medium-6 large-4 cell info-card box-shadow">
                     <div class="grid-x grid-padding-x">
-                        <div class="large-4 medium-4 cell">
-                            <p><a href="http://foundation.zurb.com/docs">Foundation Documentation</a><br />Everything you need to know about using the framework.</p>
+                        <div class="small-2 cell">
+                            <i class="far fa-calendar-alt"></i>
                         </div>
-                        <div class="large-4 medium-4 cell">
-                            <p><a href="http://zurb.com/university/code-skills">Foundation Code Skills</a><br />These online courses offer you a chance to better understand how Foundation works and how you can master it to create awesome projects.</p>
-                        </div>
-                        <div class="large-4 medium-4 cell">
-                            <p><a href="http://foundation.zurb.com/forum">Foundation Forum</a><br />Join the Foundation community to ask a question or show off your knowledge.</p>
+                        <div class="small-10 cell">
+                            <div class="card-content">
+                                <p>December 20, 2020</p>
+                            </div>
                         </div>
                     </div>
+                </div>
+
+                <div class="small-12 medium-6 large-4 cell info-card box-shadow">
                     <div class="grid-x grid-padding-x">
-                        <div class="large-4 medium-4 medium-push-2 cell">
-                            <p><a href="http://github.com/zurb/foundation">Foundation on Github</a><br />Latest code, issue reports, feature requests and more.</p>
+                        <div class="small-2 cell">
+                            <i class="far fa-clock" id ="big-icon"></i>
                         </div>
-                        <div class="large-4 medium-4 medium-pull-2 cell">
-                            <p><a href="https://twitter.com/ZURBfoundation">@zurbfoundation</a><br />Ping us on Twitter if you have questions. When you build something with this we'd love to see it (and send you a totally boss sticker).</p>
+                        <div class="small-10 cell">
+                            <div class="card-content">
+                                <p>9:00am / 11:00am</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="grid-x grid-padding-x">
-            <div class="large-8 medium-8 cell">
-                <h5>Here&rsquo;s your basic grid:</h5>
-                <!-- Grid Example -->
+                <div class="small-12 medium-6 large-4 cell info-card-long box-shadow">
+                    <div class="grid-x grid-padding-x">
+                        <div class="small-2 cell">
+                            <i class="fas fa-map-marker-alt"></i>
+                        </div>
+                        <div class="small-10 cell">
+                            <div class="card-content">
+                                <p>1551 W Lamar Alexander Pkwy</p>
+                                <p>Maryville TN, 37801</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                <div class="grid-x grid-padding-x">
-                    <div class="large-12 cell">
-                        <div class="primary callout">
-                            <p><strong>This is a twelve cell section in a grid-x.</strong> Each of these includes a div.callout element so you can see where the cell are - it's not required at all for the grid.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="grid-x grid-padding-x">
-                    <div class="large-6 medium-6 cell">
-                        <div class="primary callout">
-                            <p>Six cell</p>
-                        </div>
-                    </div>
-                    <div class="large-6 medium-6 cell">
-                        <div class="primary callout">
-                            <p>Six cell</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="grid-x grid-padding-x">
-                    <div class="large-4 medium-4 small-4 cell">
-                        <div class="primary callout">
-                            <p>Four cell</p>
-                        </div>
-                    </div>
-                    <div class="large-4 medium-4 small-4 cell">
-                        <div class="primary callout">
-                            <p>Four cell</p>
-                        </div>
-                    </div>
-                    <div class="large-4 medium-4 small-4 cell">
-                        <div class="primary callout">
-                            <p>Four cell</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="large-4 medium-4 cell">
-                <h5>Try one of these buttons:</h5>
-                <p><a href="#" class="button">Simple Button</a><br/>
-                    <a href="#" class="success button">Success Btn</a><br/>
-                    <a href="#" class="alert button">Alert Btn</a><br/>
-                    <a href="#" class="secondary button">Secondary Btn</a></p>
-                <div class="callout">
-                    <h5>So many components, girl!</h5>
-                    <p>A whole kitchen sink of goodies comes with Foundation. Check out the docs to see them all, along with details on making them your own.</p>
-                    <a href="http://foundation.zurb.com/sites/docs/" class="small button">Go to Foundation Docs</a>
-                </div>
+
             </div>
         </div>
     </div>
-</div>
+<!--    <div class="full-width main-background">
+        <div class="grid-container">
+            <div class="grid-x grid-padding-x">
+                <div class="cell-outer">
+                    <div class="small-12 medium-4 cell info-card">
+                        <div class="grid-x grid-padding-x">
+                            <div class="small-3 cell">
+                                <i class="far fa-clock"></i>
+                            </div>
+                            <div class="small-9 cell">
+                                <p>1551 W Lamar Alexander Pkwy</p>
+                                <p>Maryville TN, 37801</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="small-12 medium-4 cell info-card">
+                        <div class="grid-x grid-padding-x">
+                            <div class="small-3 cell">
+                                <i class="far fa-clock"></i>
+                            </div>
+                            <div class="small-9 cell">
+                                <p>1551 W Lamar Alexander Pkwy</p>
+                                <p>Maryville TN, 37801</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>-->
 
 
 
