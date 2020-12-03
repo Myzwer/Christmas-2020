74ffc6f6cf6ade8811621a0c83c7fe6a95545739
Myzwer <josh@onefortyfivedesign.com>
2020-12-02 23:17:51 -0500

    Updated ACF on the homepage to make everything dynamic.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -19,14 +19,14 @@ get_header(); ?>
     FAQ -->
 
     <div class="show-for-desktop">
-        <video class = "" src="http://christmas-2020.local/wp-content/uploads/2020/11/Christmas-Backround.mp4" autoplay loop
+        <video class = "" src="<?php the_field('video'); ?>" autoplay loop
                playsinline muted></video>
 
         <div class="viewport-header">
             <div class="main-header">
                 <div class="logo"><img
-                        src="http://christmas-2020.local/wp-content/uploads/2020/12/AFC_WEBSITE_SCALED_TRANSPARENT-1.png"
-                        alt="">
+                        src="<?php the_field('logo'); ?>"
+                        alt="A Foothills Christmas Logo">
                 </div>
             </div>
         </div>
@@ -38,7 +38,7 @@ get_header(); ?>
             <div class="grid-x grid-padding-x full-background" style = "background: linear-gradient(
                     rgba(255, 255, 255, .45),
                     rgba(255, 255, 255, .45)
-                    ),url(http://christmas-2020.local/wp-content/uploads/2020/12/Christmas-mobile.jpg);
+                    ),url(<?php the_field('photo_background'); ?>);
                     background-position: center center;
                     background-repeat: no-repeat;
                     background-attachment: scroll;
@@ -47,7 +47,7 @@ get_header(); ?>
                     <div class="content-middle width-large">
                         <div class="viewport-header">
                             <div class="head-container">
-                                <div class="logo"><img src="http://christmas-2020.local/wp-content/uploads/2020/12/AFC_WEBSITE_SCALED_TRANSPARENT-1.png">
+                                <div class="logo"><img src="<?php the_field('logo'); ?>">
                                 </div>
                             </div>
                         </div>
@@ -67,7 +67,7 @@ get_header(); ?>
                         </div>
                         <div class="small-10 cell">
                             <div class="card-content">
-                                <p>December 20, 2020</p>
+                                <p><?php the_field('date'); ?></p>
                             </div>
                         </div>
                     </div>
@@ -80,7 +80,7 @@ get_header(); ?>
                         </div>
                         <div class="small-10 cell">
                             <div class="card-content">
-                                <p>9:00am / 11:00am</p>
+                                <p><?php the_field('times'); ?></p>
                             </div>
                         </div>
                     </div>
@@ -93,8 +93,7 @@ get_header(); ?>
                         </div>
                         <div class="small-10 cell">
                             <div class="card-content">
-                                <p>1551 W Lamar Alexander Pkwy</p>
-                                <p>Maryville TN, 37801</p>
+                                <?php the_field('address'); ?>
                             </div>
                         </div>
                     </div>
@@ -107,12 +106,12 @@ get_header(); ?>
         <div class = "add-medium-padding">
             <div class="grid-x grid-padding-x grid-margin-x">
                 <div class="small-12 medium-6 large-8 cell">
-                    <h1 class = "light-color-invert">A Foothills Christmas</h1>
-                    <p class = "tb-padding">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet beatae blanditiis consectetur eum ex exercitationem facere, id ipsam iure laborum libero modi numquam perferendis perspiciatis provident sapiente, sequi voluptatibus.</p>
+                    <h1 class = "light-color-invert"><?php the_field('section_title_1'); ?></h1>
+                    <p class = "tb-padding"><?php the_field('section_copy_1'); ?></p>
                 </div>
                 
                 <div class="small-12 medium-6 large-4 small-padding center">
-                    <img class = "box-shadow" src="http://christmas-2020.local/wp-content/uploads/2020/12/Christmas-FIller-Photo.jpg" alt="">
+                    <img class = "box-shadow" src="<?php the_field('filler_photo'); ?>" alt="">
                 </div>
             </div>
         </div>
@@ -122,18 +121,34 @@ get_header(); ?>
         <div class="grid-x grid-padding-x full-background banner-background" style = "background: linear-gradient(
                     rgba(255, 0, 0, .85),
                     rgba(255, 0, 0, .85)
-                    ),url(http://christmas-2020.local/wp-content/uploads/2020/12/christmas-online-background.jpg);
+                    ),url(<?php the_field('background_image'); ?>);
                     background-position: left center;
                     background-repeat: no-repeat;
                     background-attachment: fixed;
                     background-size: cover;">
             <div class="grid-container">
                 <div class="small-12 cell ">
-                    <h2 class = "dark-color-invert margin-top padding-top">A Foothills Christmas Online</h2>
-                    <p class = "dark-color-invert tb-padding margin-bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet beatae blanditiis consectetur eum ex exercitationem facere, id ipsam iure laborum libero modi numquam perferendis perspiciatis provident sapiente, sequi voluptatibus.</p>
-                    <a href="https://live.foothillschurch.online/">
-                        <button class="btn btn-v1 margin-bottom">Watch Online</button>
-                    </a>
+                    <h2 class = "dark-color-invert margin-top padding-top"><?php the_field('section_title_2'); ?></h2>
+                    <p class = "dark-color-invert tb-padding margin-bottom"><?php the_field('section_copy_2'); ?></p>
+                    <?php if( have_rows('button_1') ): ?>
+                        <?php while( have_rows('button_1') ): the_row(); ?>
+                            <a href = "<?php the_sub_field('button_link'); ?>">
+                                <button class="btn btn-v1 margin-bottom"><?php the_sub_field('button_text'); ?></button>
+                            </a>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                </div>
+                <hr>
+                <div class="small-12 cell ">
+                    <h2 class = "dark-color-invert margin-top padding-top"><?php the_field('section_title_3'); ?></h2>
+                    <p class = "dark-color-invert tb-padding margin-bottom"><?php the_field('section_copy_3'); ?></p>
+                    <?php if( have_rows('button_2') ): ?>
+                        <?php while( have_rows('button_2') ): the_row(); ?>
+                            <a href = "<?php the_sub_field('button_link'); ?>">
+                                <button class="btn btn-v1 margin-bottom"><?php the_sub_field('button_text'); ?></button>
+                            </a>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -143,49 +158,47 @@ get_header(); ?>
         <div class = "add-medium-padding">
             <div class="grid-x grid-padding-x grid-margin-x">
                 <div class="small-12 center cell">
-                    <h1 class = "light-color-invert add-padding">FAQ</h1>
+                    <h1 class = "light-color-invert add-padding"><?php the_field('section_title_4'); ?></h1>
                 </div>
 
                 <div class="small-12 medium-6 large-4 small-padding center">
-                    <img class = "box-shadow" src="http://christmas-2020.local/wp-content/uploads/2020/12/AFC_INSTA_1-copy.jpg" alt="">
+                    <img class = "box-shadow" src="<?php the_field('filler_photo_2'); ?>" alt="">
                 </div>
 
                 <div class="small-12 medium-6 large-8 cell">
                     <div class="faq-content">
-                        <div class="faq-question">
-                            <input id="q1" type="checkbox" class="panel">
-                            <div class="plus">+</div>
-                            <label for="q1" class="panel-title">Is there parking on site?</label>
-                            <div class="panel-content">42</div>
-                        </div>
+                        <?php
+                        // Start the Repeater Loop
+                        if( have_rows('qaa') ):
+                            // Card Start
+                            // Begin looping data
+                            $num = 1;
+                            while ( have_rows('qaa') ) : the_row();
+                                echo '<div class="faq-question">';
+                                echo '<input id="q';
+                                echo $num;
+                                echo '"type="checkbox" class="panel">';
+                                    echo '<div class="plus">+</div>';
 
-                        <div class="faq-question">
-                            <input id="q2" type="checkbox" class="panel">
-                            <div class="plus">+</div>
-                            <label for="q2" class="panel-title">How long will the experience last?</label>
-                            <div class="panel-content">A woodchuck would chuck all the wood he could chuck, if a woodchuck could chuck wood!</div>
-                        </div>
+                                    echo '<label for="q';
+                                    echo $num;
+                                    echo '" class="panel-title">';
+                                    the_sub_field('question');
+                                    echo '</label>';
 
-                        <div class="faq-question">
-                            <input id="q3" type="checkbox" class="panel">
-                            <div class="plus">+</div>
-                            <label for="q3" class="panel-title">What is there to do for my kids?</label>
-                            <div class="panel-content">Certain questions are better left &nbsp; <a href="https://en.wikipedia.org/wiki/The_Unanswered_Question" target="_blank">unanswered</a></div>
-                        </div>
+                                    echo '<div class="panel-content">';
+                                    the_sub_field('answer');
+                                    echo '</div>';
+                                echo '</div>';
+                                $num++;
 
-                        <div class="faq-question">
-                            <input id="q3" type="checkbox" class="panel">
-                            <div class="plus">+</div>
-                            <label for="q3" class="panel-title">What should I wear?</label>
-                            <div class="panel-content">Certain questions are better left &nbsp; <a href="https://en.wikipedia.org/wiki/The_Unanswered_Question" target="_blank">unanswered</a></div>
-                        </div>
+                            endwhile;
+                        else :
+                            // no rows found
+                        endif;
+                        ?>
 
-                        <div class="faq-question">
-                            <input id="q3" type="checkbox" class="panel">
-                            <div class="plus">+</div>
-                            <label for="q3" class="panel-title">Are masks required?</label>
-                            <div class="panel-content">Certain questions are better left &nbsp; <a href="https://en.wikipedia.org/wiki/The_Unanswered_Question" target="_blank">unanswered</a></div>
-                        </div>
+
                     </div>
                 </div>
             </div>
